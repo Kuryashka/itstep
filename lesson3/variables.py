1bbc39197a3b27ef70787c6122297aacfd40a412
Kuryashka <78092503+Kuryashka@users.noreply.github.com>
2021-02-28 19:12:22 +0200

    Add files via upload

---
+++ lesson3/variables.py
@@ -0,0 +1,19 @@
+current_year = int(2021)
+
+light_speed_in_vacuum = float(299792.458)
+
+quote = '"Можливо все, неможливе просто потребує більше часу" \nДен Браун'
+
+oceans = ['Pacific', 'Atlantic', 'Indian', 'Southern', 'Arctic']
+
+active = False
+
+naming = ('i', 't', 's', 't', 'e', 'p')
+
+
+print(current_year)
+print(light_speed_in_vacuum)
+print(quote)
+print(oceans)
+print(active == False)
+print(naming)
