ce75019268f0f78b05a36d56782304ab53801c36
Kuryashka <78092503+Kuryashka@users.noreply.github.com>
2021-03-05 01:00:42 +0200

    Update rot13.py

--- lesson7/rot13.py
+++ lesson7/rot13.py
@@ -6,17 +6,17 @@ def rot13(s):
 
         if c >= ord('a') and c <= ord('z'):
             if c > ord('m'):
-                c -= 7
+                c -= 13 
 
             else:
-                c += 7
+                c += 13
 
         elif c >= ord('A') and c <= ord('Z'):
             if c > ord('M'):
-                c -= 7
+                c -= 13
 
             else:
-                c += 7
+                c += 13
 
         result += chr(c)
 
