6ad799d3c90cb2c5c917ad8bc4c1557581a66696
Kuryashka <78092503+Kuryashka@users.noreply.github.com>
2021-04-07 19:53:16 +0300

    Add files via upload

---
+++ lesson21/country.py
@@ -0,0 +1,42 @@
+class Country:
+    
+    def __init__(self, name, capital, continent, population, cities_names):
+        self.name = name
+        self.capital = capital
+        self.continent = continent
+        self.population = population
+        self.cities_names = cities_names
+
+    def __str__(self):
+        return f'country name : {self.name}\ncapital : {self.capital}\ncontinent : {self.continent}\npopulation : {self.population}\ncities_names : {self.cities_names}'
+
+
+try:
+    name = input("Enter country's name: ")
+except ValueError:
+        print("Enter valid type")       
+try:
+    capital = input("Enter capital's name: ")
+except ValueError:
+    print("Enter valid type")
+    
+try:
+    continent = input("Enter continent's name: ")
+except ValueError:
+    print("Enter valid type")
+    
+try:
+    population = float(input("Enter population amount: "))
+except ValueError:
+    print("Enter valid type")
+    
+try:
+    cities_names = input("Enter cities' names: ")
+except ValueError:
+    print("Enter valid type")
+         
+
+country = Country(name, capital, continent, population, cities_names)
+print(country)
+
+        
