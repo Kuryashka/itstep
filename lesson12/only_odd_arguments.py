367853bf30ba13eed94c1778ead82691e38d1886
Kuryashka <78092503+Kuryashka@users.noreply.github.com>
2021-05-18 23:34:25 +0300

    Create only_odd_arguments.py

---
+++ lesson12/only_odd_arguments.py
@@ -0,0 +1,24 @@
+def only_add_numbers(func):
+    z = False
+    def wrapper(*args):
+        for i in args:
+            if i % 2 == 0:
+                print('Please, choose odd arguments!')
+                return z
+            return func(args)
+    return wrapper
+
+@only_add_numbers
+def add(args):
+    return sum(list(args))
+
+print(add(5, 5))
+
+@only_add_numbers
+def multiply(args):
+    m = 1
+    for i in list(args):
+        m *= i
+    return m
+
+print(multiply(5, 5, 5))
